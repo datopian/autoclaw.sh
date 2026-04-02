56a15ed131a4e3b4104ae500688dd23bf5a7f0e5
mayor <anuar.ustayev@gmail.com>
2026-04-02 12:08:05 +0500

    Remove unused redirect worker (using Cloudflare Redirect Rule instead)

--- redirects/autopilot/src/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request) {
-    const url = new URL(request.url);
-    return Response.redirect(
-      `https://autoclaw.sh${url.pathname}${url.search}`,
-      301
-    );
-  },
-};
